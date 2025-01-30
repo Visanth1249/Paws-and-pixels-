83f71d0dfa870eb615d461b7ae25f4ed29d6e15b
unknown <visanthkonakala@gmail.com>
2025-01-30 19:45:26 +0530

    Prepare for Render deployment

--- paws/settings.py
+++ paws/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zy(jkf3$ay&su%x%$4))eb@4xd(n3lpw@(&i^vp7%g%028txwh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Paws and Pixels.onrender.com"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'paws.urls'
